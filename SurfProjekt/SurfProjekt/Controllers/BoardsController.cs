7ac4501d5db33edc1a2d286709ff20190a25729a
Iben Svare <ibensvare@gmail.com>
2022-09-29 13:13:34 +0200

    AddedAdminAndUserRoles

--- SurfProjekt/SurfProjekt/Controllers/BoardsController.cs
+++ SurfProjekt/SurfProjekt/Controllers/BoardsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,6 +14,9 @@ using SurfProjekt.Models.ViewModels;
 
 namespace SurfProjekt.Controllers
 {
+    
+    [Authorize(Roles = $"{ConstantsRole.Roles.Admin}, {ConstantsRole.Roles.User}")]
+    [AllowAnonymous]
     public class BoardsController : Controller
     {
         private UserManager<IdentityUser> userManager;
