8d9599597c7bb2704c791ccafc770ecec9c53c22
Christina-stokbak <94115578+Christina-stokbak@users.noreply.github.com>
2022-11-14 12:53:41 +0100

    Blazor component for details + authentication attempt

--- SurfProjekt/SurfProjektBlazor/Server/Program.cs
+++ SurfProjekt/SurfProjektBlazor/Server/Program.cs
@@ -38,6 +38,12 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseCors(cors => cors
+.AllowAnyMethod()
+.AllowAnyHeader()
+.SetIsOriginAllowed(origin => true)
+.AllowCredentials()
+);
 
 app.UseHttpsRedirection();
 
